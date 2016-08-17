560ccb127bd1a971258652012ea86addaff053ca
matthieu88160 <matthieu.vallance@cscfa.fr>
2016-08-17 14:05:36 +0200

    Adding MapCollection class

---
+++ src/CSDT/CollectionsBundle/Collections/MapCollection.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the Hephaistos project management API.
+ * 
+ * As each files provides by the CSCFA, this file is licensed
+ * under the MIT license.
+ *
+ * PHP version 5.6
+ *
+ * @category Collection
+ * @package  Hephaistos
+ * @author   matthieu vallance <matthieu.vallance@cscfa.fr>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     http://cscfa.fr
+ */
+namespace CSDT\CollectionsBundle\Collections;
+
+use CSDT\CollectionsBundle\AbstractCollections\AbstractMapCollection;
+
+/**
+ * Map collection
+ *
+ * This class is used to create
+ * a collection of assiociative
+ * keys values.
+ *
+ * @category Collection
+ * @package  Hephaistos
+ * @author   matthieu vallance <matthieu.vallance@cscfa.fr>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     http://cscfa.fr
+ */
+class MapCollection extends AbstractMapCollection
+{
+}
